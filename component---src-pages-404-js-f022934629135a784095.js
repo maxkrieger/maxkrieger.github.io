6b0039700d7e9535c7bd81d3f781a24946506952
maxkrieger <themaxaxis@gmail.com>
2021-01-29 00:58:06 -0500

    Updates

---
+++ component---src-pages-404-js-f022934629135a784095.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),o=n("Bl7J"),r=n("vrFN");t.default=function(){return l.a.createElement(o.a,null,l.a.createElement(r.a,{title:"404: Not found"}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"Sorry - feel free to contact me if you're looking for something. Keep this URL handy!"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-f022934629135a784095.js.map
