8d38f5dc5ec49723551f4adcd09434c5395368e2
Max Krieger <maxkrieger@users.noreply.github.com>
2024-01-15 12:03:06 -0800

    add email

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import ProjectBox from "../components/projectbox"
 import voicelinerIcon from "../images/voiceliner.png"
 import voicelinerCover from "../images/voiceliner-cover.png"
@@ -28,122 +28,125 @@ import SEO from "../components/seo"
 import Callout from "../components/callout"
 import TinyList from "../components/tinyList"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <p style={{ fontWeight: 400 }}>
-      I care about enabling human flourishing at the planetary level, with
-      universal access to abundance, beauty, and longevity. My background is in
-      interaction design and implementation. I'd like to make learning hard
-      things indistinguishable from doing them for real.
-    </p>
-    {/* <Callout
+function IndexPage() {
+  useEffect(() => {
+    setTimeout(() => {
+      document.getElementById("horn").innerHTML = atob("b2kuOWFAeGFt")
+        .split("")
+        .reverse()
+        .join("")
+    }, 200)
+  }, [])
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <p style={{ fontWeight: 400 }}>
+        I care about enabling human flourishing at the planetary level, with
+        universal access to abundance, beauty, and longevity. My background is
+        in interaction design and implementation. I'd like to make learning hard
+        things indistinguishable from doing them for real.
+      </p>
+      {/* <Callout
       outer="I'm open for freelance work in design, product, research, and devrel."
       inner="I'm growing in these areas and would love to work with thoughtful, supportive teams. Particularly with  projects in creative and scientific spaces. Reach out if you've been working on something for a while and are looking for a fresh perspective! I'm based in San Francisco for a while and especially love collaborating in-person."
     /> */}
-    <p>
-      📯{" "}
-      <pre style={{ display: "inline", padding: 0 }}>
-        (contact via twitter DM)
-      </pre>
-    </p>
-    <p>
-      Besides Twitter, you can find some other half-baked thoughts on my{" "}
-      <a href="/inquiry">inquiry page</a>.
-    </p>
-    <section>
-      <h2 className="divider">present</h2>
-      <ProjectBox
-        title="catchMe"
-        kind={"get caught"}
-        link="https://catchme.life"
-        external={true}
-        bg={catchmeCover}
-        icon={catchmeIcon}
-      />
-    </section>
-    <section>
-      <h2 className="divider">tiny stuff</h2>
-      <TinyList links={[["Cell Feed", "https://a9.io/gramstract/"]]} />
-    </section>
-    <section>
-      <h2 className="divider">past</h2>
-      <ProjectBox
-        title="Spaceshifter"
-        kind={"tune embeddings"}
-        link="/spaceshifter/"
-        external={true}
-        bg={spaceshifterCover}
-        icon={spaceshifterIcon}
-      />
-      <ProjectBox
-        title="Voiceliner"
-        kind={"voice notes app"}
-        link="/voiceliner/"
-        external={true}
-        bg={voicelinerCover}
-        icon={voicelinerIcon}
-      />
-      <ProjectBox
-        title="Penrose"
-        kind={"diagramming tool"}
-        link="https://penrose.ink"
-        external={true}
-        bg={penroseScreen}
-        icon={penroseIcon}
-      />
-      <ProjectBox
-        title="Chatting with Glue"
-        kind={"comic"}
-        external={true}
-        link="http://a9.io/glue-comic/"
-        bg={salienceMap}
-        icon={sagaIcon}
-      />
-      <ProjectBox
-        title="How to see the unseeable"
-        link="/seeing-the-unseeable"
-        bg={unseeable}
-        kind={"visualization exercise"}
-        icon={playdoh}
-      />
-      <ProjectBox
-        title="Where is my mind? (situated)"
-        kind={"fan music video"}
-        link="https://vimeo.com/389667501"
-        bg={mindBg}
-        external={true}
-        icon={turtle}
-      />
-      <ProjectBox
-        title="LaTeX Complete"
-        kind={"figma plugin"}
-        link="https://www.figma.com/community/plugin/793023817364007801/LaTeX-Complete"
-        bg={latexBg}
-        external={true}
-        icon={latexIcon}
-      />
-      <ProjectBox
-        title="Defining Visual Narratives for Mathematics Declaratively"
-        kind={"workshop paper"}
-        link="krieger_plateau_2019.pdf"
-        bg={narrative}
-        external={true}
-        icon={comicIcon}
-      />
-    </section>
-    <section>
-      <h2 className="divider">old</h2>
-      <ProjectBox
-        title="LiquidTime"
-        kind={"time tool"}
-        link="https://liquidti.me"
-        bg={liquidBg}
-        external={true}
-        icon={liquidIcon}
-      />
-    </section>
-  </Layout>
-)
+      <p style={{ textAlign: "right", marginTop: -20 }}>
+        <span id="horn" style={{ color: "rgba(0,0,0,0.6)" }}></span>
+      </p>
+      <section>
+        <h2 className="divider">present</h2>
+        <ProjectBox
+          title="catchMe"
+          kind={"get caught"}
+          link="https://catchme.life"
+          external={true}
+          bg={catchmeCover}
+          icon={catchmeIcon}
+        />
+      </section>
+      <section>
+        <h2 className="divider">tiny stuff</h2>
+        <TinyList links={[["Cell Feed", "https://a9.io/gramstract/"]]} />
+      </section>
+      <section>
+        <h2 className="divider">past</h2>
+        <ProjectBox
+          title="Spaceshifter"
+          kind={"tune embeddings"}
+          link="/spaceshifter/"
+          external={true}
+          bg={spaceshifterCover}
+          icon={spaceshifterIcon}
+        />
+        <ProjectBox
+          title="Voiceliner"
+          kind={"voice notes app"}
+          link="/voiceliner/"
+          external={true}
+          bg={voicelinerCover}
+          icon={voicelinerIcon}
+        />
+        <ProjectBox
+          title="Penrose"
+          kind={"diagramming tool"}
+          link="https://penrose.ink"
+          external={true}
+          bg={penroseScreen}
+          icon={penroseIcon}
+        />
+        <ProjectBox
+          title="Chatting with Glue"
+          kind={"comic"}
+          external={true}
+          link="http://a9.io/glue-comic/"
+          bg={salienceMap}
+          icon={sagaIcon}
+        />
+        <ProjectBox
+          title="How to see the unseeable"
+          link="/seeing-the-unseeable"
+          bg={unseeable}
+          kind={"visualization exercise"}
+          icon={playdoh}
+        />
+        <ProjectBox
+          title="Where is my mind? (situated)"
+          kind={"fan music video"}
+          link="https://vimeo.com/389667501"
+          bg={mindBg}
+          external={true}
+          icon={turtle}
+        />
+        <ProjectBox
+          title="LaTeX Complete"
+          kind={"figma plugin"}
+          link="https://www.figma.com/community/plugin/793023817364007801/LaTeX-Complete"
+          bg={latexBg}
+          external={true}
+          icon={latexIcon}
+        />
+        <ProjectBox
+          title="Defining Visual Narratives for Mathematics Declaratively"
+          kind={"workshop paper"}
+          link="krieger_plateau_2019.pdf"
+          bg={narrative}
+          external={true}
+          icon={comicIcon}
+        />
+      </section>
+      <section>
+        <h2 className="divider">old</h2>
+        <ProjectBox
+          title="LiquidTime"
+          kind={"time tool"}
+          link="https://liquidti.me"
+          bg={liquidBg}
+          external={true}
+          icon={liquidIcon}
+        />
+      </section>
+    </Layout>
+  )
+}
 
 export default IndexPage
