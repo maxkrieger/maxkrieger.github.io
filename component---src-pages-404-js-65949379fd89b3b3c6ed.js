0f0fe6acf03f96e966216837d8b9d89482e625c5
Max Krieger <maxkrieger@users.noreply.github.com>
2024-04-12 16:43:15 -0700

    Updates

---
+++ component---src-pages-404-js-65949379fd89b3b3c6ed.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[125],{3331:function(e,t,l){l.r(t);var a=l(6540),n=l(2269);t.default=()=>a.createElement(a.Fragment,null,a.createElement(n.A,{title:"404: Not found"}),a.createElement("h1",null,"NOT FOUND"),a.createElement("p",null,"Sorry - feel free to contact me if you're looking for something. Keep this URL handy!"))}}]);
+//# sourceMappingURL=component---src-pages-404-js-65949379fd89b3b3c6ed.js.map
